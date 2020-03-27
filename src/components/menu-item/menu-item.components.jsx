ccdb92473e2cf0d20226e2db2862ed46688a9db9
rmsparajuli <rameshparajuli09@gmail.com>
2020-03-27 10:32:35 +0545

    Routing crwn-cloting

--- src/components/menu-item/menu-item.components.jsx
+++ src/components/menu-item/menu-item.components.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./menu-item.styles.scss";
+import { withRouter } from "react-router-dom";
 // const MenuItem = props => (
 //   <div className="menu-item">
 //     {console.log("props on menu item", props)}
@@ -12,8 +13,11 @@ import "./menu-item.styles.scss";
 
 // or we can do it by this kind of direct props name method using ({ props_name })
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)} //  /somematchedURL/linkURL
+  >
     <div
       style={{
         backgroundImage: `url(${imageUrl})`
@@ -27,4 +31,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
