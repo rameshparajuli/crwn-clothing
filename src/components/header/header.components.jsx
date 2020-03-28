8ff0aa6f46da989452cdfd83687682bf4b9c31f6
rmsparajuli <rameshparajuli09@gmail.com>
2020-03-28 14:14:54 +0545

    header component is created

---
+++ src/components/header/header.components.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/crown.svg.svg";
+// ReactComponent is a special syntax in React for importing SVG. Next lesson will explain how this works
+import "./header.styles.scss";
+
+const Header = () => (
+  <div className="header">
+    <Link to="/" className="logo-contain">
+      <Logo className="logo" />
+    </Link>
+
+    <div className="options">
+      <Link to="/shop" className="option">
+        SHOP
+      </Link>
+      <Link to="/contact" className="option">
+        CONTACT
+      </Link>
+    </div>
+  </div>
+);
+
+export default Header;
