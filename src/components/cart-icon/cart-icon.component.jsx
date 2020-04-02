d1c6c5cfeeab7bb709e2a56b13167aa2f55833e4
rmsparajuli <rameshparajuli09@gmail.com>
2020-04-03 00:46:04 +0545

    updating our application with reselect, replaced cart selectors with memorized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
 
+import { selectCartItemsCount } from "../../redux/cart/cart.selector";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
@@ -17,4 +18,14 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = state => ({
+  // itemCount: cartItems.reduce(
+  //   (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity,
+  //   0
+  // )
+  // we replace with selectorCart because we create custom for this
+  itemCount: selectCartItemsCount(state)
+});
+
+/** cartItems.reduce(())   is a native array method in JavaScript */
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
