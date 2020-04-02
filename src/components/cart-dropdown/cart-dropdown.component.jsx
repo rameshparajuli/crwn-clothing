9c97d44d5e1bf8ef2efc933a98d5339c9de3df46
rmsparajuli <rameshparajuli09@gmail.com>
2020-04-02 08:10:58 +0545

    create cart-item and implement redux

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import CustomButton from "../custom-button/custom-button.component";
+
+import "./cart-dropdown.styles.scss";
+
+const CartDropdown = () => (
+  <div className="cart-dropdown">
+    <div className="cart-items">
+      <p>hello</p>
+      <p>hello</p>
+      <p>hello</p>
+      <p>hello</p>
+      <p>hello</p>
+      <p>hello</p>
+      <p>hello</p>
+      <p>hello</p>
+      <p>hello</p>
+      <p>hello</p>
+      <p>hello</p>
+    </div>
+    <CustomButton>GO TO CHECKOUT</CustomButton>
+  </div>
+);
+
+export default CartDropdown;
--- src/components/header/header.components.jsx
+++ src/components/header/header.components.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import { ReactComponent as Logo } from "../../assets/crown.svg.svg";
 // ReactComponent is a special syntax in React for importing SVG. Next lesson will explain how this works
 import "./header.styles.scss";
 import { auth } from "../../API/firebase/firebase.utils";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     {console.log("props on header", currentUser)}
     <Link to="/" className="logo-contain">
@@ -30,12 +32,16 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 });
 // mapStateToProps naming can be anything but mapStateToProps is standard with redux codebases
 export default connect(mapStateToProps)(Header);
