fc1d1f35df3aadf1f2e7291b532a28ed7f5c0a36
rmsparajuli <rameshparajuli09@gmail.com>
2020-04-03 15:30:49 +0545

    create user selector and implement add to cart on carticon

--- src/components/header/header.components.jsx
+++ src/components/header/header.components.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import { selectCartHidden } from "../../redux/cart/cart.selector";
+import { selectCurrentUser } from "../../redux/user/user.selector";
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import { ReactComponent as Logo } from "../../assets/crown.svg.svg";
@@ -38,9 +42,9 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 );
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-  currentUser,
-  hidden
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden
 });
 // mapStateToProps naming can be anything but mapStateToProps is standard with redux codebases
 export default connect(mapStateToProps)(Header);
