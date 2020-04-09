ee48948f24f5057d3e5cb1b28f99749f1dd47e9e
rmsparajuli <rameshparajuli09@gmail.com>
2020-04-09 14:14:18 +0545

    host on heroku and remove redux-logger from prd.

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,14 @@ import logger from "redux-logger"; // logger for middleware
 import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 const persistor = persistStore(store);
 
-export  { store, persistor };
+export { store, persistor };
