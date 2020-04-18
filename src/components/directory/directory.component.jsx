7ec646f7ab73d5225d2d8eeb78e61b131e378e81
rmsparajuli <rameshparajuli09@gmail.com>
2020-04-18 09:56:58 +0545

    change css for make responsive on mobile

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -6,12 +6,14 @@ import { selectDirectorySections } from "../../redux/directory/directory.selecto
 import "./directory.styles.scss";
 import MenuItem from "../menu-item/menu-item.components";
 
+import { DirectoryMenuContainer } from "./directory.styles";
+
 const Directory = ({ sections }) => (
-  <div className="directory-menu">
+  <DirectoryMenuContainer>
     {sections.map(({ id, ...otherSectionProps }) => (
       <MenuItem key={id} {...otherSectionProps} />
     ))}
-  </div>
+  </DirectoryMenuContainer>
 );
 const mapStateToProps = createStructuredSelector({
   sections: selectDirectorySections,
--- src/pages/homepage/homepage.styles.jsx
+++ src/pages/homepage/homepage.styles.jsx
@@ -3,5 +3,5 @@ export const HomePageContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 20px 80px;
+  /* padding: 20px 80px; */
 `;
