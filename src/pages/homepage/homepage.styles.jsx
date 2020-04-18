fbdc1aa9d6b12c262a680523622e5504e60501ac
rmsparajuli <rameshparajuli09@gmail.com>
2020-04-18 10:07:27 +0545

    change paddin on homepage

--- src/pages/homepage/homepage.styles.jsx
+++ src/pages/homepage/homepage.styles.jsx
@@ -3,5 +3,8 @@ export const HomePageContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  /* padding: 20px 80px; */
+  padding: 20px 80px;
+  @media screen and (max-width: 800px) {
+    padding: unset;
+  }
 `;
