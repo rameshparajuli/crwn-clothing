19d8f18d4293632758e4b3c2429bb2433b2234a1
rmsparajuli <rameshparajuli09@gmail.com>
2020-04-21 09:01:24 +0545

    implement redux-saga on shop and create shop saga and root saga

---
+++ src/redux/root-saga.js
@@ -0,0 +1,5 @@
+import { all, call } from "redux-saga/effects";
+import { fetchCollectionsStart } from "./shop/shop.sagas";
+export default function* rootSaga() {
+  yield all([yield all([call(fetchCollectionsStart)])]);
+}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,25 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 import logger from "redux-logger";
-import thunk from "redux-thunk";
+// import thunk from "redux-thunk";
+// we don't need it now cause we are using sagaMiddleware now
+import createSagaMiddleware from "redux-saga";
+
+import rootSaga from "./root-saga";
 
 import rootReducer from "./root-reducer";
 
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
